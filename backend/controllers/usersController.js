410e98d3bd57316d946741f418e5573875ebc479
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-12-06 10:54:11 +0530

    Searching, Filtering & pagination added to backend

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -3,8 +3,27 @@ const User = require('../model/User');
 // Get all users
 const getUsers = async (req, res) => {
     try {
-        const users = await User.find();
-        if (!users) return res.status(204).json({ "message": "No Employees found" })
+        const { gender, domain, available, name } = req.query;
+        const query = {};
+        if (gender) query.gender = gender;
+        if (domain) query.domain = domain;
+        if (available) query.available = available;
+        if (name) {
+            query.$or = [
+                { first_name: { $regex: name, $options: 'i' } },
+                { last_name: { $regex: name, $options: 'i' } }
+            ]
+        }
+
+        let users = await User.find(query);
+
+        if (users.length == 0) return res.status(204).json({ "message": "No Employees found" })
+
+        const page = Number(req?.query?.page) || 1;
+        const limit = Number(req?.query?.limit) || 20;
+        const skip = (page - 1) * limit;
+        users = users.slice(skip, skip + limit);
+
         res.json(users);
     } catch (err) {
         console.error(err.message);
