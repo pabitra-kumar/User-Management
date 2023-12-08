bfa6a097800d05f9ffbfeb91f903d159b9e832ba
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-12-08 23:27:12 +0530

    minor bug!

--- frontend/src/Routes/AddUser.jsx
+++ frontend/src/Routes/AddUser.jsx
@@ -25,7 +25,7 @@ export const AddUser = () => {
         } catch (error) {
             console.error('rejected: ', error);
         }
-        navigate("/users")
+        navigate("/")
     };
 
     return (
@@ -75,7 +75,7 @@ export const AddUser = () => {
                     </div>
                     <div className="buttons flex flex-wrap justify-between">
                         {isLoading ? "Loading..." : <button onClick={addUser} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Add User</button>}
-                        <Link to="/users">
+                        <Link to="/">
                             <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Cancel</button>
                         </Link>
                     </div>
--- frontend/src/Routes/EditUser.jsx
+++ frontend/src/Routes/EditUser.jsx
@@ -34,7 +34,7 @@ export const EditUser = () => {
         } catch (error) {
             console.error('rejected: ', error);
         }
-        navigate("/users")
+        navigate("/")
     };
 
     return (
@@ -82,8 +82,8 @@ export const EditUser = () => {
                         <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I am available</label>
                     </div>
                     <div className="buttons flex flex-wrap justify-between">
-                        {isLoading ? "Loading..." : <button onClick={updateUser} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Add User</button>}
-                        <Link to="/users">
+                        {isLoading ? "Loading..." : <button onClick={updateUser} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Edit User</button>}
+                        <Link to="/">
                             <button type="submit" className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Cancel</button>
                         </Link>
                     </div>
